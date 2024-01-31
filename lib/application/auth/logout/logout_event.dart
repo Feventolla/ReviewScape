0cc929cca801dee5b0f48a08cfeed3dcc5136f28
Rediet-Ferew <redietasnakech@gmail.com>
2024-01-31 09:58:31 +0300

    Adding application layer

--- lib/application/auth/logout/logout_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'logout_bloc.dart';
-
-@immutable
-abstract class LogoutEvent extends Equatable {}
-
-class LogoutRequested extends LogoutEvent {
-  @override
-  List<Object?> get props => [];
-}
